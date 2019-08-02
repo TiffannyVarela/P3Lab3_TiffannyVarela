e7453e351cb7c8ec9fcc849c3a76a3e18f063688
TiffannyVarela <tiffanny_varela529@unitec.edu>
2019-08-02 16:11:43 -0600

    Main

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<iomanip> 
+#include <string.h>
+#include <vector>
+#include <bits/stdc++.h>
+
+int menu();
+
+int main(){
+	int opc = 0;
+		
+	do{
+		switch(opc=menu()){
+			case 1:
+				break;
+		}
+	}while(opc!=);
+	return 0;
+}
